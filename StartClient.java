c236a3dbc3829286cb7730dfa887be164ea80a5c
johneklof <eklof.john@gmail.com>
2017-03-07 13:10:02 +0100

    Uppdaterad viewer
    Lagt till fält för aktiva användare och utökat textfältet

--- StartClient.java
+++ StartClient.java
@@ -10,23 +10,14 @@ public class StartClient {
 	public StartClient(){
 		//String username = JOptionPane.showInputDialog("Välj ett användarnamn");
 		Client client;
-		try {
-			client = new Client("127.0.0.1", 3672, "bög");
-			new Controller(client);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		client = new Client("127.0.0.1", 3672, "bög");
+		new Controller(client);
 		
 	}
 	
 	public static void main(String[] args) {
-		//new StartClient();
-		//Client client;
-		try{
-			//client = new Client("127.0.0.1", 3672, "bög");
-			//new Controller(client);
-			new Controller(new Client("127.0.0.1",3520, "bög"));
-		}catch(IOException e){}
+		//client = new Client("127.0.0.1", 3672, "bög");
+		//new Controller(client);
+		new Controller(new Client("127.0.0.1",3520, "bög"));
 	}
 }
--- Viewer.java
+++ Viewer.java
@@ -9,24 +9,31 @@ public class Viewer extends JPanel implements ActionListener{
 	private ClientController controller;
 	private JTextField messageInput = new JTextField();
 	private JTextArea messageArea = new JTextArea();
+	private JTextArea messageUsers = new JTextArea ("AKTIVA ANVÄNDARE");
 	private JButton btnSend = new JButton("Skicka");
 	private JPanel panelSouth = new JPanel();
 	private JPanel panelCenter = new JPanel();
+	private JPanel panelWest = new JPanel ();
+	
 	
 	public Viewer(ClientController controller){
 		this.controller = controller;
 		setLayout(new BorderLayout());
 		messageArea.setPreferredSize(new Dimension(500,500));
-		messageInput.setPreferredSize(new Dimension(200,50));
+		messageInput.setPreferredSize(new Dimension(450,100));
+		messageUsers.setPreferredSize(new Dimension(150,500));
 		btnSend.setPreferredSize(new Dimension(75,50));
 		panelSouth.add(messageInput, BorderLayout.CENTER);
 		panelSouth.add(btnSend, BorderLayout.EAST);
 		panelCenter.add(messageArea, BorderLayout.CENTER);
+		panelWest.add(messageUsers, BorderLayout.CENTER);
 		add(panelCenter,BorderLayout.CENTER);
 		add(panelSouth, BorderLayout.SOUTH);
+		add(panelWest, BorderLayout.WEST);
 		messageInput.addActionListener(this);
 		btnSend.addActionListener(this);
 		messageArea.setEditable(false);
+		messageUsers.setEditable(false);
 	}
 	
 	public void updateChat(Object o){
