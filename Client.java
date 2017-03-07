89c7c7f9e4ba70b70dc852becc234956e906ef30
DavidSandh <kung.sandh@gmail.com>
2017-03-07 09:25:51 +0100

    Mergeconflict
    merge

--- Client.java
+++ Client.java
@@ -23,7 +23,7 @@ public class Client {
 		new Connection(ip, port).start();
 		// connect();
 	}
-
+//DET E NAJS MED GITFAN
 	public void connect() throws UnknownHostException, IOException {
 		System.out.println("connect metoden i klient");
 		socket = new Socket(ip, port);
