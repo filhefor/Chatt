1d0d83b657427c4c3266c9632215203dfe3ac40e
DavidSandh <kung.sandh@gmail.com>
2017-03-07 13:16:13 +0100

    Extended ThreadPool class
    Extended ThreadPool class

--- ThreadPool.java
+++ ThreadPool.java
@@ -1,18 +1,65 @@
 package gu;
 
-public class ThreadPool {
+import java.util.LinkedList;
 
-	
-	private class Buffer<T>{
-		
+public class ThreadPool {
+	private Buffer<Runnable> buffer = new Buffer<Runnable>();
+	private LinkedList<Worker> workerList;
+	private int maxThreads;
+
+	public ThreadPool(int maxThreads) {
+		this.maxThreads = maxThreads;
 	}
-	private class Worker extends Thread{
-		public void run(){
-			while(!Thread.interrupted()){
-				buffer.get().run();
-			}catch (InterruptedException interruptedException){
-				interruptedException.printStackTrace();
-				break;
+
+	public synchronized void start() {
+		Worker worker;
+		if(workerList == null){
+			workerList = new LinkedList<Worker>();
+			for(int i = 0; i < maxThreads; i++){
+				worker = new Worker();
+				worker.start();
+				workerList.add(worker);
+			}
+		}
+
+	}
+	public void execute(Runnable runnable){
+		buffer.put(runnable);
+	}
+
+	private class Buffer<T> {
+		private LinkedList<T> buffer = new LinkedList<T>();
+
+		public synchronized void put(T obj) {
+			buffer.addLast(obj);
+			notifyAll();
+		}
+
+		public synchronized T get() throws InterruptedException {
+			while (buffer.isEmpty()) {
+				wait();
+			}
+			return buffer.removeFirst();
+		}
+
+		public synchronized void clear() {
+			buffer.clear();
+		}
+
+		public int size() {
+			return buffer.size();
+		}
+	}
+
+	private class Worker extends Thread {
+		public void run() {
+			while (!Thread.interrupted()) {
+				try {
+					buffer.get().run();
+				} catch (InterruptedException interruptedException) {
+					interruptedException.printStackTrace();
+					break;
+				}
 			}
 		}
 	}
