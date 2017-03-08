77f8faa59ded156abc8be979d7d5486875dac450
DavidSandh <kung.sandh@gmail.com>
2017-03-08 17:37:04 +0100

    Fixat scrollpane
    Scrollpane

--- Viewer.java
+++ Viewer.java
@@ -4,47 +4,48 @@ import java.awt.*;
 import java.awt.event.*;
 
 import javax.swing.*;
-import javax.swing.border.EtchedBorder;
 
-public class Viewer extends JPanel implements ActionListener{
+public class Viewer extends JPanel implements ActionListener, KeyListener{
+	
 	private ClientController controller;
+	
 	private JTextField messageInput = new JTextField();
-	private JTextArea messageArea = new JTextArea();
-	private JTextArea messageUsers = new JTextArea ("AKTIVA ANVÄNDARE");
+	private JTextArea messageArea = new JTextArea();	
+	private JTextArea connectedUsers= new JTextArea();
+
 	private JButton btnSend = new JButton("Skicka");
+	
 	private JPanel panelSouth = new JPanel();
 	private JPanel panelCenter = new JPanel();
 	private JPanel panelWest = new JPanel ();
-	
+	private JScrollPane scrollPane = new JScrollPane(messageArea);
 	
 	public Viewer(ClientController controller){
 		this.controller = controller;
-		setPreferredSize(new Dimension(800,600));
+		setPreferredSize(new Dimension(700,600));
 		setLayout(new BorderLayout());
 		
-		messageArea.setPreferredSize(new Dimension(500,10));
-		messageInput.setPreferredSize(new Dimension(450,100));
-		messageUsers.setPreferredSize(new Dimension(150,500));
-		btnSend.setPreferredSize(new Dimension(75,50));
+		messageInput.setPreferredSize(new Dimension(450, 100));
+		scrollPane.setPreferredSize(new Dimension(400,400));
+		connectedUsers.setPreferredSize(new Dimension(150,450));
+		connectedUsers.setBorder(BorderFactory.createLineBorder(Color.BLACK));
 		
+		
+		panelWest.add(connectedUsers, BorderLayout.CENTER);
+		panelCenter.add(scrollPane, BorderLayout.CENTER);
 		panelSouth.add(messageInput, BorderLayout.CENTER);
 		panelSouth.add(btnSend, BorderLayout.EAST);
-//		panelCenter.add(new JScrollPane(messageArea), BorderLayout.CENTER);
-//		panelCenter.add(messageArea, BorderLayout.CENTER);
-//		panelWest.add(messageUsers, BorderLayout.CENTER);
-
-		add(messageArea, BorderLayout.CENTER);;
-		add(messageUsers, BorderLayout.WEST);
-		add(panelSouth, BorderLayout.SOUTH);
 		
-		messageArea.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.LOWERED));
-		messageUsers.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.RAISED));
 		
-		messageInput.addActionListener(this);
+		messageInput.addKeyListener(this);
 		btnSend.addActionListener(this);
 		messageArea.setEditable(false);
-		messageUsers.setEditable(false);
-
+		connectedUsers.setEditable(false);
+		
+		add(panelWest, BorderLayout.WEST);
+		add(panelSouth, BorderLayout.SOUTH);
+		add(panelCenter, BorderLayout.CENTER);
+		
 	}
 	
 	public void updateChat(Object o){
@@ -65,5 +66,25 @@ public class Viewer extends JPanel implements ActionListener{
 		System.out.println("Du klickade på enter/skicka");
 		controller.sendObject((Object)messageInput.getText());
 		messageInput.setText("");
+	}
+	
+	public void keyPressed(KeyEvent e){
+	    if(e.getKeyCode() == KeyEvent.VK_ENTER){
+	    e.consume();
+	    btnSend.doClick();
+	    }
+	
+}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 }
